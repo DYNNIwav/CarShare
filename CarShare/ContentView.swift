da7103753c094787b65645232a21046bcb5145ec
Pål Omland Eilevstjønn <paalomei@gmail.com>
2024-11-21 23:53:46 +0100

    Initial Commit

---
+++ CarShare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarShare
+//
+//  Created by Pål Omland Eilevstjønn on 21/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CarShareTests/CarShareTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarShareTests.swift
+//  CarShareTests
+//
+//  Created by Pål Omland Eilevstjønn on 21/11/2024.
+//
+
+import Testing
+@testable import CarShare
+
+struct CarShareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
